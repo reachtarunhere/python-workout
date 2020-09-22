2760f0222900c1690029e6345d8faf34865e0b9b
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-09-23 04:55:56 +0800

    my solution to guessing_game

--- ch01-numbers/e01_guessing_game.py
+++ ch01-numbers/e01_guessing_game.py
@@ -1,1 +1,21 @@
+import random
 
+
+def guessing_game():
+
+    chosen_number = random.randint(0, 100)
+
+    for i in range(3):
+        guess = int(input('Guess a number: '))
+        if guess > chosen_number:
+            print('Too High')
+        elif guess < chosen_number:
+            print('Too Low')
+        else:
+            print('Just Right')
+            break
+
+
+if __name__ == '__main__':
+
+    guessing_game()
