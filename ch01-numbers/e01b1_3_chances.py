88246507fbd50f4d18378fe4c980994ada77aceb
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-09-23 04:56:21 +0800

    guess withing 3 times

--- ch01-numbers/e01b1_3_chances.py
+++ ch01-numbers/e01b1_3_chances.py
@@ -1,1 +1,24 @@
+import random
 
+
+def guessing_game():
+
+    chosen_number = random.randint(0, 100)
+
+    for i in range(3):
+        guess = int(input('Guess a number: '))
+        if guess > chosen_number:
+            print('Too High')
+        elif guess < chosen_number:
+            print('Too Low')
+        else:
+            print('Just Right')
+            break
+
+    if guess != chosen_number:
+        print("You didn't guess in time")
+
+
+if __name__ == '__main__':
+
+    guessing_game()
