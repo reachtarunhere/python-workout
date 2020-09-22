ee231d9778396da1bfb5ec670c97e06557dac8fb
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-09-23 04:50:47 +0800

    emptied all src files to write my own code instead

--- ch02-strings/e05_pig_latin.py
+++ ch02-strings/e05_pig_latin.py
@@ -1,13 +1,1 @@
-#!/usr/bin/env python3
-"""Solution to chapter 2, exercise 5: pig_latin"""
 
-
-def pig_latin(word):
-    """Translates a word into Pig Latin.
-The "word" parameter is assumed to be an
-English word, returned as a string.
-"""
-    if word[0] in 'aeiou':
-        return f'{word}way'
-
-    return f'{word[1:]}{word[0]}ay'
--- ch02-strings/e08_sort_string.py
+++ ch02-strings/e08_sort_string.py
@@ -1,9 +1,1 @@
-#!/usr/bin/env python3
-"""Solution to chapter 2, exercise 8: strsort"""
 
-
-def strsort(a_string):
-    """Takes a string as input,
-returns a string with its characters sorted.
-"""
-    return ''.join(sorted(a_string))
