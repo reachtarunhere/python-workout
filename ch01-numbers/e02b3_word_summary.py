605948ad66b1a7188fac69043e30c2e8087ee2eb
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-09-23 18:42:04 +0800

    implement word_summary

--- ch01-numbers/e02b3_word_summary.py
+++ ch01-numbers/e02b3_word_summary.py
@@ -1,1 +1,9 @@
+from statistics import mean
 
+
+def word_summary(words):
+    word_lens = [len(w) for w in words]
+    return (min(word_lens), max(word_lens), mean(word_lens))
+
+
+print(word_summary(["what", "a", "day"]))
