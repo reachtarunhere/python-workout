140ebf2b611a80a90af5ee2c7b95005f5afb550f
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-09-23 17:33:14 +0800

    guess number with random base

--- ch01-numbers/e01b2_guess_base.py
+++ ch01-numbers/e01b2_guess_base.py
@@ -1,1 +1,23 @@
+import random
 
+
+def guessing_game():
+
+    random_base = random.randint(2, 16)
+    chosen_number = random.randint(0, 100)
+
+    while True:
+        guess = int(input(f'Guess a number in base {random_base}: '),
+                    base=random_base)
+        if guess > chosen_number:
+            print('Too High')
+        elif guess < chosen_number:
+            print('Too Low')
+        else:
+            print('Just Right')
+            break
+
+
+if __name__ == '__main__':
+
+    guessing_game()
