53ae9911b0cf6d46c0dde147f14d32430a79f7f8
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-09-23 19:13:36 +0800

    first pass implementation of sum_intable

--- ch01-numbers/e02b4_sum_intable.py
+++ ch01-numbers/e02b4_sum_intable.py
@@ -1,1 +1,12 @@
+def sum_intable(items):
 
+    def make_int(i):
+        try:
+            return int(i)
+        except TypeError:
+            return 0
+
+    return sum(make_int(i) for i in items)
+
+
+print(sum_intable([1, 2.4, float]))
