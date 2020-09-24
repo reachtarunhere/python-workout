832692c31f3888b771bcd5a0c226d15bf20b8cf6
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-09-24 12:47:56 +0800

    run timing solution

--- ch01-numbers/e03_run_timing.py
+++ ch01-numbers/e03_run_timing.py
@@ -1,1 +1,12 @@
+from statistics import mean
 
+
+def run_timing():
+    times = []
+    while True:
+        user_input = input("Enter 10 km time: ")
+        if user_input:
+            times.append(float(user_input))
+        else:
+            break
+    print(f"Average of {mean(times)}, over {len(times)} runs")
