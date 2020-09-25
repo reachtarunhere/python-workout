6c679e4f7301423f3c40d1609d52f4d3515819f0
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-09-25 21:32:19 +0800

    remove useless breakdown into steps after looking model solution

--- ch01-numbers/e03b1_before_after.py
+++ ch01-numbers/e03b1_before_after.py
@@ -1,9 +1,7 @@
 def before_after(num, before, after):
     num_str = str(num)
     decimal_index = num_str.find('.')
-    before_digits = num_str[decimal_index-before:decimal_index]
-    after_digits = num_str[decimal_index:decimal_index+after+1]
-    return float(before_digits+after_digits)
+    return float(num_str[decimal_index-before:decimal_index+after+1])
 
 
 print(before_after(1234.5678, 2, 3))
