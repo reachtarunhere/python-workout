cf7948065442ed166789afc567c974c573548d04
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-09-26 02:49:12 +0800

    solved hex_output

--- ch01-numbers/e04_hex_output.py
+++ ch01-numbers/e04_hex_output.py
@@ -1,1 +1,6 @@
-
+def hex_output():
+    hex_str = input("enter hexadecimal number")
+    integer = 0
+    for position, digit in enumerate(reversed(hex_str)):
+        integer += int(digit, base=16) * 16 ** position
+    print(integer)
