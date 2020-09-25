c3cfd1b1efa6d345d94b5400669e4fadc65a567c
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-09-25 21:28:37 +0800

    before_after own implementation

--- ch01-numbers/e03b1_before_after.py
+++ ch01-numbers/e03b1_before_after.py
@@ -1,1 +1,9 @@
+def before_after(num, before, after):
+    num_str = str(num)
+    decimal_index = num_str.find('.')
+    before_digits = num_str[decimal_index-before:decimal_index]
+    after_digits = num_str[decimal_index:decimal_index+after+1]
+    return float(before_digits+after_digits)
 
+
+print(before_after(1234.5678, 2, 3))
