bed8fd8e4919100268fb17bf7dc9a6262746a4b6
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-09-29 01:15:09 +0800

    pig_latin

--- ch02-strings/e05b1_capitalized.py
+++ ch02-strings/e05b1_capitalized.py
@@ -1,1 +1,18 @@
+def copy_case(src, tgt):
+    if src.isupper():
+        return tgt.upper()
+    elif src.istitle():
+        return tgt.title()
+    return tgt.lower()
 
+
+def pig_latin(word):
+    output = None
+    if word[0] in "aeiou":
+        output = word + "way"
+    else:
+        output = word[1:] + word[0] + "ay"
+    return copy_case(word, output)
+
+
+print(pig_latin("Tarun"))
