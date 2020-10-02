849c38ca438f0898348b67ff36f5d429957bf2cf
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-10-02 09:12:58 +0800

    more than one vowel pig latin

--- ch02-strings/e05b3_different_vowels.py
+++ ch02-strings/e05b3_different_vowels.py
@@ -1,1 +1,15 @@
+def pig_latin_two_or_more(word):
 
+    def two_or_more_vowels():
+        return len(set(word).intersection(set("aeiou"))) >= 2
+
+    if two_or_more_vowels():
+        output = word + "way"
+    else:
+        output = word[1:] + word[0] + "ay"
+
+    return output
+
+
+print(pig_latin_two_or_more("wine"))
+print(pig_latin_two_or_more("wind"))
