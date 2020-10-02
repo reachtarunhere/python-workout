25f425ad932f5de07391d4f506152096f75491ef
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-10-02 08:54:12 +0800

    pig latin with punctuation

--- ch02-strings/e05b2_punctuation.py
+++ ch02-strings/e05b2_punctuation.py
@@ -1,1 +1,19 @@
+from string import punctuation
 
+
+def pig_latin_punctuation(word):
+
+    if word[-1] in punctuation:
+        word, end_symbol = word[:-1], word[-1]
+    else:
+        word, end_symbol = word, ""
+
+    if word[0] in "aeiou":
+        output = word + "way"
+    else:
+        output = word[1:] + word[0] + "ay"
+
+    return output + end_symbol
+
+
+print(pig_latin_punctuation("tarun!"))
