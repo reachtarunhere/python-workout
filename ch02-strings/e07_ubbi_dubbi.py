c934c5b34e9a3f285899b1aa56ae3e6a83d05b9c
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2020-10-11 22:17:50 +0800

    ubbi_dubbi replace based solution

--- ch02-strings/e07_ubbi_dubbi.py
+++ ch02-strings/e07_ubbi_dubbi.py
@@ -1,1 +1,5 @@
-
+def ubbi_dubbi(word):
+    # u first becaue other charcters will introduce u's that we shouldn't replace
+    for vowel in 'uaeio':
+        word = word.replace(vowel, f'ub{vowel}')
+    return word
